a08525ea9d5ca27209fabe2bbf21a490d32d80b5
VochanecA <126856875+VochanecA@users.noreply.github.com>
2024-11-06 12:36:06 +0100

    Update page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -20,15 +20,13 @@ export default function HomePage() {
                 essential integrations.
               </p>
               <div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-                <a
-                  href="https://vercel.com/templates/next.js/next-js-saas-starter"
-                  target="_blank"
-                >
-                  <Button className="bg-white hover:bg-gray-100 text-black border border-gray-200 rounded-full text-lg px-8 py-4 inline-flex items-center justify-center">
-                    Deploy your own
-                    <ArrowRight className="ml-2 h-5 w-5" />
-                  </Button>
-                </a>
+              <a href="https://razglas-tiv.vercel.app/flights">
+  <Button className="bg-white hover:bg-gray-100 text-black border border-gray-200 rounded-full text-lg px-8 py-4 inline-flex items-center justify-center">
+    View flights
+    <ArrowRight className="ml-2 h-5 w-5" />
+  </Button>
+</a>
+
               </div>
             </div>
             <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
