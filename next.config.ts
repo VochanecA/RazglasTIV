2d3b7841c05d55ae7eadbf2c80ad504cf9aa3639
VochanecA <126856875+VochanecA@users.noreply.github.com>
2024-11-06 11:34:23 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
-// Import the necessary Next.js types
-import { NextConfig } from 'next';
-import { Configuration } from 'webpack';
+import type { NextConfig } from 'next';
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  // Enable if you need to serve audio files
-  webpack: (config: Configuration) => {
-    config.module?.rules?.push({
-      test: /\.(mp3)$/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/media/[name][ext]',
-      },
-    });
-    return config;
+  experimental: {
+    ppr: true,
   },
 };
 
