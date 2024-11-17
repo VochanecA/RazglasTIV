545f1af87da71c7398d1708151dc5f504557f26b
VochanecA <126856875+VochanecA@users.noreply.github.com>
2024-11-17 16:51:37 +0100

    Update flight.ts

--- types/flight.ts
+++ types/flight.ts
@@ -1,19 +1,19 @@
 export interface Flight {
-    ident: string;
-    status: string;
-    scheduled_out: string;
-    estimated_out: string;
-    actual_out: string | null;
-    origin: { code: string };
-    destination: { code: string };
-    grad:  string ;
-    Kompanija: string;
-    KompanijaICAO: string;
-    KompanijaNaziv: string;
-    checkIn: string;
-    gate: string;
-  }
-  
+  ident: string;
+  status: string;
+  scheduled_out: string;
+  estimated_out: string;
+  actual_out: string | null;
+  actual_in: string | null;  // Add this line
+  origin: { code: string };
+  destination: { code: string };
+  grad: string;
+  Kompanija: string;
+  KompanijaICAO: string;
+  KompanijaNaziv: string;
+  checkIn: string;
+  gate: string;
+}
   export interface FlightData {
     departures: Flight[];
     arrivals: Flight[];
